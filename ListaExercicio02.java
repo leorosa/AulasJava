eed42ff46fd2a7195432156487a61cc7ffd762ea
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-04-02 15:51:06 -0300

    exercicio08

--- ListaExercicio02.java
+++ ListaExercicio02.java
@@ -11,7 +11,21 @@ public class ListaExercicio02 {
 //		exercicio04();
 //		exercicio05();
 //		exercicio06();
-		exercicio07();
+//		exercicio07();
+//		exercicio08();
+//		exercicio09();
+//		exercicio10();
+//		exercicio11();
+//		exercicio12();
+//		exercicio13();
+//		exercicio14();
+//		exercicio15();
+//		exercicio16();
+//		exercicio17();
+//		exercicio18();
+//		exercicio19();
+//		exercicio20();
+		exercicio();
 	}
 	
 	static void exercicio01() {
@@ -116,6 +130,78 @@ public class ListaExercicio02 {
 		} else {
 			System.out.println("nota excelente.");
 		}
+		sc.close();
+	}
+
+	static void exercicio08() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("informe o comprimento de três lados: ");
+		float lado1 = sc.nextFloat();
+		float lado2 = sc.nextFloat();
+		float lado3 = sc.nextFloat();
+		if (lado1>lado2) {
+			if (lado1>lado3) { 	// lado1 é o maior
+				if (lado2+lado3 > lado1) {
+					System.out.println("os 3 lados formam um triângulo.");
+				}
+			} else {			// lado3 é o maior
+				if (lado1+lado2 > lado3) {
+					System.out.println("os 3 lados formam um triângulo.");
+				}
+			}
+		} else {
+			if (lado2>lado3) { 	// lado2 é o maior
+				if (lado1+lado3 > lado2) {
+					System.out.println("os 3 lados formam um triângulo.");
+				}
+			} else {			// lado3 é o maior
+				if (lado1+lado2 > lado3) {
+					System.out.println("os 3 lados formam um triângulo.");
+				}
+			}
+		}
+		sc.close();
+	}
+
+
+	static void exercicio() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("informe o comprimento de três lados: ");
+		float lado1 = sc.nextFloat();
+		float lado2 = sc.nextFloat();
+		float lado3 = sc.nextFloat();
+		String tipo = "";
+// ideia: usar resposta do exercicio08 para checar se é um triângulo 
+		if (lado1==lado2 || lado1==lado3 || lado2==lado3) {
+			tipo = "isósceles";
+		} else {
+// testar se é retângulo
+			if (lado1>lado2) {
+				if (lado1>lado3) { 	// lado1 é o maior
+					if (lado2*lado2+lado3*lado3 == lado1*lado1) {
+						tipo = "retângulo";
+					}
+				} else {			// lado3 é o maior
+					if (lado1*lado1+lado2*lado2 == lado3*lado3) {
+						tipo = "retângulo";
+					}
+				}
+			} else {
+				if (lado2>lado3) { 	// lado2 é o maior
+					if (lado1*lado1+lado3*lado3 == lado2*lado2) {
+						tipo = "retângulo";
+					}
+				} else {			// lado3 é o maior
+					if (lado1*lado1+lado2*lado2 == lado3*lado3) {
+						tipo = "retângulo";
+					}
+				}
+			}
+		}
+		if (tipo=="") {
+			tipo = "escaleno";
+		}
+		System.out.println("(se for um triângulo) é " + tipo);
 		sc.close();
 	}
 
