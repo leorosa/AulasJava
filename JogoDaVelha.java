4ec5b1ba8f736d4c4fb46adbc3c274c6685ee7d9
leorosa <leorosa@gmail.com>
2026-04-10 09:39:49 -0300

    outras melhorias (rótulos, argumentos)

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,30 +1,26 @@
 import java.util.Scanner;
 
 public class JogoDaVelha {
-    public static char[][] status = {{'¹','²','³'} , {'⁴','⁵','⁶'} , {'⁷','⁸','⁹'}};
+    public static char[][] status = {{' ',' ',' '} , {' ',' ',' '} , {' ',' ',' '}};
+    public static char[][] rotulos = {{'¹','²','³'} , {'⁴','⁵','⁶'} , {'⁷','⁸','⁹'}};
+	public static char[] jogadores = {'X','O'};
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		char[] jogadores = {'X','O'};
 		int pos = 0;
-		int x = 0;
-		int y = 0;
 		int jogador = 0;
 		imprimeStatus();
 		while (true) {
 			while (true) {
 				System.out.println(jogadores[jogador] + ", indique posição: ");
 				pos = sc.nextInt() - 1; // 1a. posição = 0
-				x = pos/3;
-				y = pos%3;
-				if (testePosicao(x, y)) {
-					status[x][y] = jogadores[jogador];
+				if (testePosicao(pos, jogador)) {
 					break;
 				} else {
 					System.out.println("posição inválida.");
 				}
 			}
 			imprimeStatus();
-			if (testeJogador(jogadores[jogador])) {
+			if (testeJogador(jogador)) {
 				System.out.println(jogadores[jogador] + " venceu");
 				break;
 			} else if (testeFim()) {
@@ -36,20 +32,24 @@ public class JogoDaVelha {
 		sc.close();
 	}
 
-	public static boolean testePosicao(int i, int j) {
-		if (0<=i && i<=2 && 0<=j && j<=2 && status[i][j]!='X' && status[i][j]!='O') {
-			return true;  // posição válida/livre
-		}
-		else { return false; }
+	public static boolean testePosicao(int pos, int jogador) {
+	    int x = pos/3;
+	    int y = pos%3;
+	    if (0<=pos && pos<=9 && status[x][y]!='X' && status[x][y]!='O') {
+			status[x][y] = jogadores[jogador];
+			rotulos[x][y] = ' ';
+			return true; // posição válida/livre
+	    } else { return false; }
 	}
 
-	public static boolean testeJogador(char jogador) {
+	public static boolean testeJogador(int jogador) {
+	    char jog = jogadores[jogador];
 		for(int i=0;i<=2;i++) {
-			if (status[i][0]==jogador && status[i][1]==jogador && status[i][2]==jogador) { return true; }
-			if (status[0][i]==jogador && status[1][i]==jogador && status[2][i]==jogador) { return true; }
+			if (status[i][0]==jog && status[i][1]==jog && status[i][2]==jog) { return true; }
+			if (status[0][i]==jog && status[1][i]==jog && status[2][i]==jog) { return true; }
 		}
-		if (status[0][0]==jogador && status[1][1]==jogador && status[2][2]==jogador) { return true; }
-		if (status[0][2]==jogador && status[1][1]==jogador && status[2][0]==jogador) { return true; }
+		if (status[0][0]==jog && status[1][1]==jog && status[2][2]==jog) { return true; }
+		if (status[0][2]==jog && status[1][1]==jog && status[2][0]==jog) { return true; }
 		return false;
 	}
 
@@ -68,7 +68,7 @@ public class JogoDaVelha {
 		System.out.print("\033[H\033[2J");	// clear console
 		System.out.flush();
 		for(int i=0;i<=2;i++) {
-			System.out.println(" " + status[i][0] + " | " + status[i][1] + " | " + status[i][2]);
+			System.out.println(" " + status[i][0] + rotulos[i][0] + "| " + status[i][1] + rotulos[i][1] + "| " + status[i][2] + rotulos[i][2]);
 			if (i<2) {
 				System.out.println("---+---+---");
 			}
