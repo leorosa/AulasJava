6aa22246b15eb5b722e5c76515cd79efda204ac8
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-04-10 16:26:22 -0300

    segundo jogador = aleatório

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class JogoDaVelha {
 
-	public static char[][] tabuleiro = { {' ', ' ', ' '} , {' ', ' ', ' '} , {' ', ' ', ' '} };
-	public static char[][] posicoes = { {'⁰', '¹', '²'} , {'³', '⁴', '⁵'} , {'⁶', '⁷', '⁸'} };
-	// {'0', '1', '2'} , {'3', '4', '5'} , {'6', '7', '8'}};
-	public static char[] jogadores = { 'X', 'O' };
+	public static char[][] tabuleiro = { {'⁰', '¹', '²'} , {'³', '⁴', '⁵'} , {'⁶', '⁷', '⁸'} }; // {'0', '1', '2'} , {'3', '4', '5'} , {'6', '7', '8'}};
+	public static char[] simbolos = { 'X', 'O' }; // representação visual dos jogadores
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Random gerador = new Random();
 		int indiceJogador = 0;
 		imprimeTabuleiro();
 		while (true) {
-			System.out.print("entre com uma posição: ");
+			System.out.print("jogador " + simbolos[indiceJogador] + ", entre com uma posição: ");
 			while (true) {
+				int pos;
 // jogador entra com posição
-				int pos = sc.nextInt();
-// testa se posição é válida
-// se for, salvar posição; senão, pedir novamente
+				if (indiceJogador==0) {
+					pos = sc.nextInt();
+				} else {
+					pos = gerador.nextInt(8);
+					System.out.println("");
+				}
+// testa se posição é válida; se for, salvar posição; senão, pedir novamente
 				if (testaPosicao(pos, indiceJogador)) {
 					break;
 				} else {
@@ -26,8 +31,8 @@ public class JogoDaVelha {
 			}
 			imprimeTabuleiro();
 // testar se jogador venceu; se sim, encerrar o jogo
-			if (testeVitoria(jogadores[indiceJogador])) {
-				System.out.println("jogador " + jogadores[indiceJogador] + " venceu.");
+			if (testeVitoria(simbolos[indiceJogador])) {
+				System.out.println("jogador " + simbolos[indiceJogador] + " venceu.");
 				break; // sai do jogo
 			}
 // testar se ainda há posições livres; se não, encerrar o jogo
@@ -36,9 +41,9 @@ public class JogoDaVelha {
 				break;
 			}
 // trocar de jogador
-//			if (jogador==0) { jogador = 1;
-//			} else { 		 jogador = 0; }
-			indiceJogador = (indiceJogador+1)%2;	// 0->1 ; 1->0
+//			if (jogador==0) { jogador = 1; } else { jogador = 0; }
+//			indiceJogador = (indiceJogador+1)%2;	// 0->1 ; 1->0
+			indiceJogador = (indiceJogador==1)?0:1;
 		}
 		sc.close();
 	}
@@ -46,7 +51,7 @@ public class JogoDaVelha {
 	public static void imprimeTabuleiro() {
 		System.out.println("");
 		for (int i=0; i<tabuleiro.length; i++) {
-			System.out.println(" " + tabuleiro[i][0] + posicoes[i][0] + "│ " + tabuleiro[i][1] + posicoes[i][1] + "│ " + tabuleiro[i][2] + posicoes[i][2]);
+			System.out.println(" " + tabuleiro[i][0] + " │ " + tabuleiro[i][1] + " │ " + tabuleiro[i][2]);
 			if (i==2) { break; }
 			System.out.println("───┼───┼───");
 		}
@@ -58,9 +63,8 @@ public class JogoDaVelha {
 		}
 		int x = pos%3;
 		int y = pos/3;
-		if (tabuleiro[y][x] != jogadores[0] && tabuleiro[y][x] != jogadores[1]) {
-			tabuleiro[y][x] = jogadores[indiceJogador];
-			posicoes[y][x] = ' ';
+		if (tabuleiro[y][x]!=simbolos[0] && tabuleiro[y][x]!=simbolos[1]) {
+			tabuleiro[y][x] = simbolos[indiceJogador];
 			return true;
 		} else {
 			return false;
@@ -80,7 +84,7 @@ public class JogoDaVelha {
 	public static boolean tabuleiroCheio() {
 		for (int i=0; i<tabuleiro.length; i++) {
 			for (int j=0; j<tabuleiro.length; j++) {
-				if (tabuleiro[i][j] != jogadores[0] && tabuleiro[i][j] != jogadores[1]) {
+				if (tabuleiro[i][j]!=simbolos[0] && tabuleiro[i][j]!=simbolos[1]) {
 					return false;
 				}
 			}
