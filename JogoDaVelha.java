658ca1f7847d71bbbf5ee7b5af502d8cc18ccaa8
leorosa <leorosa@gmail.com>
2026-04-11 11:08:23 -0300

    permitir 2 jogadores atuados por computador

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -6,7 +6,7 @@ public class JogoDaVelha {
 	public static Scanner sc = new Scanner(System.in);
 	public static Random gerador = new Random();
 	public static char[][] tabuleiro = { {'⁰', '¹', '²'} , {'³', '⁴', '⁵'} , {'⁶', '⁷', '⁸'} }; // {'0', '1', '2'} , {'3', '4', '5'} , {'6', '7', '8'}};
-	public static char[] simbolos = { 'C', 'H', 'X', 'O' }; // representação visual dos jogadores; apenas os 2 primeiros são usados; 'C'=computador
+	public static char[] simbolos = { 'C', 'c', 'X', 'O' }; // representação visual dos jogadores; apenas os 2 primeiros são usados; 'C'=computador; 'A'=aleatório
 
 	public static void main(String[] args) {
 		int indiceJogador = 0;
@@ -16,7 +16,7 @@ public class JogoDaVelha {
 			while (true) {
 				int pos;
 // entrada de posições
-				if (simbolos[indiceJogador]=='C') {  // computador joga
+				if (simbolos[indiceJogador]=='C' || simbolos[indiceJogador]=='c' ) {  // computador joga
 					pos = jogada(indiceJogador);
 				} else if (simbolos[indiceJogador]=='A') {  // jogada aleatória
 					pos = gerador.nextInt(8);
