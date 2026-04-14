ead38bd0f453698053c5cb5c957edcc4b816dfae
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-04-14 15:49:02 -0300

    exemplo aula

---
+++ aula.js
@@ -0,0 +1,16 @@
+function variaveis() {
+    const nome = 'ana'
+    var idade = 10
+    let altura = 1.69
+
+    console.log(nome)
+    console.log(idade)
+    console.log(altura)
+}
+
+function ola() {
+    console.log('ola')
+    variaveis()
+}
+
+ola()
