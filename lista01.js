e33566f2e1bc0da8a50dac06f21e9e32ef936c62
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-04-15 13:50:39 -0300

    lista01 - exercicios 01 a 04

---
+++ lista01.js
@@ -0,0 +1,28 @@
+// 1
+
+function exercicio01() {
+    numero = 10
+    console.log(numero)
+}
+
+function exercicio02() {
+    nome = 'Leonardo'
+    idade = 48
+    altura = 1.80
+    console.log(nome)
+    console.log(idade)
+    console.log(altura)
+}
+
+function exercicio03() {
+    i1 = 2
+    i2 = 1
+    console.log(i1+i2)
+}
+
+function exercicio04() {
+    preco = 90.90
+    console.log('o preço do produto é R$'+preco)
+}
+
+exercicio04()
