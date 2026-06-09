f039f42be579e27070b10c8110197662e8ba947e
leorosa <leorosa@gmail.com>
2026-06-09 16:39:23 -0300

    permitir reproduzir comandos; ajustes na formatação
    Recreated the database and tables, and reinserted data.

--- exemplo.sql
+++ exemplo.sql
@@ -1,6 +1,10 @@
+drop database exemplo;
+create database exemplo;
+use exemplo;
+
 create table tb_categorias (
-id int primary key auto_increment,
-nome varchar(25) not null
+	id int primary key auto_increment,
+	nome varchar(25) not null
 );
 insert into tb_categorias(nome) value('frutas');
 insert into tb_categorias(nome) value('cereais');
@@ -12,8 +16,8 @@ create table tb_produtos (
 	preco decimal(6,2)
 );
 
-insert into tb_produtos(descricao,preco) values('milho', 4500.85);
-insert into tb_produtos(descricao) values('tomate');
+insert into tb_produtos(descricao,preco) values ('milho', 4500.85);
+insert into tb_produtos(descricao) values ('tomate');
 update tb_produtos
 	set preco = 1.50
 	where id = 2;
@@ -21,7 +25,7 @@ select * from tb_produtos;
 
 alter table tb_produtos add column id_categoria int;
 alter table tb_produtos add constraint fk_categoria
-	foreign key (id_categoria)
-	references tb_categorias(id);
-insert into tb_produtos(descricao,preco,id_categoria) values('maçã', 4.5, 1);
+foreign key (id_categoria)
+references tb_categorias(id);
+insert into tb_produtos (descricao,preco,id_categoria) values ('maçã', 4.5, 1);
 select * from tb_produtos;
