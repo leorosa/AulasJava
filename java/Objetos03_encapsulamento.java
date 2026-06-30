5244454d318f31aaa671174d2ae16eb3f76614ff
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-06-30 16:43:45 -0300

    deixar setters mais legíceis

--- java/Objetos03_encapsulamento.java
+++ java/Objetos03_encapsulamento.java
@@ -112,8 +112,27 @@ class Produto {
     public int getEstoque() { return this.estoque; }
     public String getUnidade() { return this.unidade; }
 
-    public void setDescricao(String descricao) { if (descricao.length()>0) this.descricao = descricao; else System.out.println("descrição inválida."); }
-    public void setPreco(double preco) { if (preco>0) this.preco = preco; else System.out.println("preço inválido."); }
-    public void setEstoque(int estoque) { if (estoque>=0) this.estoque = estoque; else System.out.println("estoque inválido."); }
-    public void setUnidade(String unidade) { if (unidade.length()>0 && unidade.length()<=2) this.unidade = unidade; else System.out.println("unidade inválida."); }
+    public void setDescricao(String descricao) {
+        if (descricao.length()>0)
+            this.descricao = descricao;
+        else
+            System.out.println("descrição inválida."); }
+    public void setPreco(double preco) {
+        if (preco>0)
+            this.preco = preco;
+        else
+            System.out.println("preço inválido.");
+    }
+    public void setEstoque(int estoque) {
+        if (estoque>=0)
+            this.estoque = estoque;
+        else
+            System.out.println("estoque inválido.");
+    }
+    public void setUnidade(String unidade) {
+        if (unidade.length()>0 && unidade.length()<=2)
+            this.unidade = unidade;
+        else
+            System.out.println("unidade inválida.");
+    }
 }
