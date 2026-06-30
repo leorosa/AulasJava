98a40b449e61c66e1a9334e8eb3b50a90408fecc
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-06-30 14:18:38 -0300

    usar menus gráficos

--- java/Objetos03.java
+++ java/Objetos03.java
@@ -13,26 +13,104 @@ Crie um programa que permita ao usuário cadastrar produtos.
 O usuário deve informar os dados. Crie uma menu com opções de inserir novo produto, 
 alterar um existente, excluir um existente, 
 listar um produto de acordo com a descrição desejado e a opção para listar todos
- */
+*/
 
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
-class Main {
+//Java class to implement AWT Menu
+//and MenuItem
+import java.awt.*;
+import java.awt.event.*;
+
+public class exercicioObjetos {
     public static Scanner sc = new Scanner(System.in); // Create a Scanner object
+    
     public static void main(String[] args) {
-        List<Produto> produtos = new ArrayList();
+        
+        Frame frame = new Frame("Menu Example");
+        MenuBar menuBar = new MenuBar();
+        frame.setMenuBar(menuBar);
+
+        // Create a "File" menu
+        Menu prodMenu = new Menu("produto");
+        MenuItem novoItem = new MenuItem("novo");
+        MenuItem alteraItem = new MenuItem("alterar");
+        MenuItem listaItem = new MenuItem("listar");
+        MenuItem listaTodos = new MenuItem("listar todos");
+        MenuItem removeItem = new MenuItem("remover");
+        MenuItem sairItem = new MenuItem("sair");
+        prodMenu.add(novoItem);
+        prodMenu.add(alteraItem);
+        prodMenu.add(listaItem);
+        prodMenu.add(listaTodos);
+        prodMenu.add(removeItem);
+        prodMenu.addSeparator();
+        prodMenu.add(sairItem);
+        menuBar.add(prodMenu);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+
+    	List<Produto> produtos = new ArrayList();
+   
+        novoItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Produto p = new Produto();
+                p.editar();
+                produtos.add(p);
+            }
+        });
+
+        alteraItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	int i = selectItem(produtos);
+            	produtos.get(i).editar();
+            }
+        });
+
+        
+        listaItem.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		int i = selectItem(produtos);
+        		produtos.get(i).display();
+        	}
+    	});
+
+        listaTodos.addActionListener(new ActionListener() {
+           	public void actionPerformed(ActionEvent e) {
+           		for (Produto p : produtos) {
+           			p.display();
+           		}
+    		}
+        });
+        
+        removeItem.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		int i = selectItem(produtos);
+        		produtos.remove(i);
+        	}
+    	});
+
+        sairItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+      
+/*
         while (true) {
             if (produtos.size()==0)
-            	System.out.println("opções: [e]ntrar novo produto");
+            	System.out.println("opções: [n]ovo produto ou [s]air");
         	else
-            	System.out.println("opções: [e]ntrar, [a]lterar, [l]istar produto, [L]istar todos, ou [r]emover");
+            	System.out.println("opções: [n]ovo produto, [a]lterar produto, [l]istar produto, [L]istar todos, [r]emover ou [s]air");
     		String opcao = sc.nextLine();
-    		if (opcao.equals("e")) {
+    		if (opcao.equals("n")) {
     	        Produto p = new Produto();
                 p.editar();
                 produtos.add(p);
+    		} else if (opcao.equals("s")) {
+    			break;
     		} else if (produtos.size()==0) {
     		    continue;
     		} else if (opcao.equals("a")) {
@@ -50,7 +128,9 @@ class Main {
     			}
     		}
         }
+*/
     }
+
 
     static int selectItem(List<Produto> produtos) {
 	    int i = 0;
