b99a051f7254e125fe3e096e85fe02b623cbe183
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-07-01 14:11:38 -0300

    exercicio03

---
+++ java/exercicioOO03.java
@@ -0,0 +1,41 @@
+public class Exercicio03 {
+	public static void main(String[] args) {
+		Aluno aluno = new Aluno();
+		aluno.setNome("João");
+		aluno.setNota1(8);
+		aluno.setNota2(7.55);
+		System.out.println(aluno.calcularMedia());
+		aluno.verificarSituacao();
+	}
+}
+
+class Aluno {
+	private String nome;
+	private double nota1;
+	private double nota2;
+
+	void setNome(String nome) {
+		if (nome.length()>0)
+			this.nome = nome;
+	}
+	void setNota1(double nota) {
+		if (nota>=0)
+			this.nota1 = nota;
+	}
+	void setNota2(double nota) {
+		if (nota>=0)
+			this.nota2 = nota;
+	}
+	double calcularMedia() {
+		return (nota1+nota2)/2;
+	}
+	void verificarSituacao() {
+		double media = calcularMedia();
+		if (media>=7)
+			System.out.println(this.nome + " está Aprovado");
+		else if (media>=5)
+			System.out.println(this.nome + " está em Recuperação");
+		else
+			System.out.println(this.nome + " está Reprovado");
+	}
+}
