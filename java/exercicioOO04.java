6e1d7123ffada6f5f8aa066835d3ffbec980b3ee
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-07-01 14:26:05 -0300

    exercicio04

---
+++ java/exercicioOO04.java
@@ -0,0 +1,34 @@
+public class Exercicio04 {
+	public static void main(String[] args) {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("João");
+		funcionario.setSalario(2500);
+		funcionario.aumentarSalario(15);
+		funcionario.calcularSalarioAnual();
+		funcionario.exibirDados();
+	}
+}
+
+class Funcionario {
+	private String nome;
+	private double salario;
+
+	void setNome(String nome) {
+		if (nome.length()>0)
+			this.nome = nome;
+	}
+	void setSalario(double salario) {
+		if (salario>0)
+			this.salario = salario;
+	}
+	void aumentarSalario(double percentual) {
+		this.salario *= (1+percentual/100);
+	}
+	void calcularSalarioAnual() {
+		System.out.println("Salário anual: " + this.salario*12);
+	}
+	void exibirDados() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("Salário: R$" + this.salario);
+	}
+}
