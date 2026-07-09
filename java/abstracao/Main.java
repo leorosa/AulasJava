0253d5920c80d170d3d1e23840b42231eb2e61d3
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-07-09 14:01:47 -0300

    exemplo abstração

---
+++ java/abstracao/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Pig p1 = new Pig();
+		p1.animalSound();
+		p1.sleep();
+		System.out.println("olá");
+	}
+}
---
+++ java/abstracao/Pig.java
@@ -0,0 +1,11 @@
+
+public class Pig extends Animal {
+	@Override
+	public void animalSound() {
+		System.out.println("oinc oing");
+	}
+	@Override
+	public void sleep() {
+		System.out.println("ronc ronc");
+	}
+}
