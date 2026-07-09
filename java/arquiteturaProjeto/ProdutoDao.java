d3c0f7f59050fbdd94c4c4edafd403eda454ebfa
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-07-09 14:59:05 -0300

    estrutura de código para projeto

---
+++ java/arquiteturaProjeto/ProdutoDao.java
@@ -0,0 +1,35 @@
+package dao;
+
+import java.util.List;
+
+import interfaces.ICRUD;
+import modelos.Produto;
+
+public class ProdutoDao implements ICRUD {
+
+	@Override
+	public Produto salvar(Produto prod) {
+		return null;
+	}
+
+	@Override
+	public void deletar(int id) {
+		
+	}
+
+	@Override
+	public void alterar(Produto prod) {
+		
+	}
+
+	@Override
+	public Produto consultar(int id) {
+		return null;
+	}
+
+	@Override
+	public List<Produto> consultar() {
+		return null;
+	}
+
+}
