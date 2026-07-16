ee0237196609398b967e48173f2751861810fb63
Leonardo Rosa <leonardo63466156@alunos.sc.senac.br>
2026-07-16 16:10:33 -0300

    variáveis renomeadas em Main para poder diferenciar produtos de clientes

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import dao.ClienteDao;
 import dao.ProdutoDao;
 import java.util.List;
 import java.util.Scanner;
@@ -6,8 +7,8 @@ import modelos.Produto;
 public class Main {
 	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		ProdutoDao dao = new ProdutoDao();
-		List<Produto> produtos = dao.consultar();
+		ProdutoDao prodDao = new ProdutoDao();
+		List<Produto> produtos = prodDao.consultar();
 		while (true) {
 			if (produtos.size()==0)
 				System.out.println("PRODUTO: [n]ovo ou [s]air");
@@ -15,9 +16,9 @@ public class Main {
 				System.out.println("PRODUTO: [n]ovo, [a]lterar, [l]istar, [L]istar todos, [r]emover ou [s]air");
 			String opcao = sc.nextLine();
 			if (opcao.equals("n")) {
-				Produto p = new Produto();
-				p.editar();
-				dao.inserir(p);
+				Produto prod = new Produto();
+				prod.editar();
+				prodDao.inserir(prod);
 			} else if (opcao.equals("s")) {
 				sc.close();
 				break;
@@ -25,26 +26,26 @@ public class Main {
 				continue;
 			} else if (opcao.equals("a")) {
 				int i = selectItem(produtos);
-				Produto p = produtos.get(i-1); // i-1 porque arrays iniciam em 0, enquanto tabelas iniciam em 1
-				p.editar();
-				dao.alterar(p);
+				Produto prod = produtos.get(i-1); // i-1 porque arrays iniciam em 0, enquanto tabelas iniciam em 1
+				prod.editar();
+				prodDao.alterar(prod);
 			} else if (opcao.equals("r")) {
 				int i = selectItem(produtos);
-				dao.deletar(i);
-				Produto p = dao.consultar(i);
-				if (p!=null) {
-					System.out.println(p.getId() + " - " + p.getDescricao() + " R$" + p.getPreco());
+				prodDao.deletar(i);
+				Produto prod = prodDao.consultar(i);
+				if (prod!=null) {
+					System.out.println(prod.getId() + " - " + prod.getDescricao() + " R$" + prod.getPreco());
 				} else {
 					System.out.println("Produto removido");
 				}
 			} else if (opcao.equals("l")) {
 				int i = selectItem(produtos);
-				Produto p = dao.consultar(i);
-				p.listar();
+				Produto prod = prodDao.consultar(i);
+				prod.listar();
 			} else if (opcao.equals("L")) {
-				List<Produto> lista = dao.consultar();
-				for(Produto p : lista) {
-					p.listar();
+				List<Produto> lista = prodDao.consultar();
+				for(Produto prod : lista) {
+					prod.listar();
 				}
 			}
 		}
