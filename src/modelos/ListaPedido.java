1cd6afffea516db6bae245700a63d6c90ddc8465
leorosa <leorosa@gmail.com>
2026-07-20 12:02:36 -0300

    adicionar objeto ListaPedido e respectivo DAO

---
+++ src/modelos/ListaPedido.java
@@ -0,0 +1,70 @@
+package modelos;
+
+import java.util.Scanner;
+
+public class ListaPedido {
+	private int id;
+	private int idPedido;
+	private int idProduto;
+	private int quantidade;
+// no banco de dados MySQL:
+//      create table lista_pedido ( id int primary key auto_increment, id_pedido int, constraint fk_pedido foreign key (id_pedido) references tb_pedidos(id), id_produto int, constraint fk_produto foreign key (id) references tb_produtos(id), quantidade int);
+
+	public ListaPedido() {
+	}
+	public ListaPedido(int idPedido, int idProduto, int quantidade) {
+		setPedido(idPedido);
+		setProduto(idProduto);
+		setQuantidade(quantidade);
+	}
+	public ListaPedido(int id, int idPedido, int idProduto, int quantidade) {
+		setId(id);
+		setPedido(idPedido);
+		setProduto(idProduto);
+		setQuantidade(quantidade);
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		if (id>0)
+			this.id = id;
+	}
+	public int getPedido() {
+		return idPedido;
+	}
+	public void setPedido(int idPedido) {
+		if (idPedido>0)
+			this.idPedido = idPedido;
+	}
+	public int getProduto() {
+		return idProduto;
+	}
+	public void setProduto(int idProduto) {
+		if (idProduto>0)
+			this.idProduto = idProduto;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		if (quantidade>0)
+			this.quantidade = quantidade;
+	}
+
+	public void listar() {
+		System.out.println(this.getId() + " - " + this.getPedido() + this.getProduto() + " " + this.getQuantidade());
+	}
+	public void editar() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("id pedido: ");
+		this.setPedido(sc.nextInt());
+		System.out.print("id produto: ");
+		this.setProduto(sc.nextInt());
+		System.out.print("quantidade: ");
+		this.setQuantidade(sc.nextInt());
+		sc.nextLine(); // consome nova linhe ignorada por sc.nextInt()
+//		sc.close();
+	}
+}
